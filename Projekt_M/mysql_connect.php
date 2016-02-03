3cf8048afa8e78a3e5b9a818c89b9446cc5fb217
Supeee <mcpro64@gmail.com>
2016-02-03 13:36:17 +0100

    yes

---
+++ Projekt_M/mysql_connect.php
@@ -0,0 +1,17 @@
+<?php
+	
+	
+	
+	
+	DEFINE('DB_USER','root');
+	DEFINE('DB_HOST','localhost');
+	DEFINE('DB_PASSWORD','xhtksdlolen123');
+	DEFINE('DB_DATABASE','SuperServ');
+ 		
+ 		
+	$dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
+
+
+
+
+?>
