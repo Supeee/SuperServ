69f1ddc6eaac62fd4c61f5a823a968f4756f099e
Supeee <mcpro64@gmail.com>
2016-02-22 16:15:10 +0100

    Nya mapper filer, etc

---
+++ MySQL_bakläxa/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="sv">
+	<head>
+		<meta charset="utf-8">
+		<title>MySQL bakläxa</title>
+	</head>
+<body>
+
+<tr>
+
+<table style="width:100%">
+
+  <tr>
+    <td>1. Vad ska man skriva om man vill få fram…</td>
+  </tr>
+
+  <tr>
+    <td>a ...kundnummer på alla som heter Per eller Peter?</td> 
+    <td> b ...förnamn på alla som har ett förnamn på tre bokstäver?</td>
+  </tr>
+
+  <tr>
+  
+  <tr>
+
+  </tr>
+
+	<td>2. Vad ska man skriva om man vill få fram förnamn och efternamn på…</td>  	
+  </tr>
+  
+  <tr>
+  	<td>a. ...alla personer utan mellannamn?</td>	
+	<td>b. ...alla personer med två eller fler mellannamn?</td>
+	<td>c. ...alla personer utan mellannamn och Per i förnamn?</td>
+  </tr>
+
+  	<td>3. Vad ska man skriva om man vill veta…</td>  	
+  </tr>
+  
+  <tr>
+  	<td>a. ...fullständigt namn på alla män?</td>
+  	</tr>
+  
+  	<tr>	
+	<td>b. ...kundnummer på alla under 66år?</td>
+	<td>c. ...kundnummer och förnamn på alla kvinnor under 35år?</td>
+	<td>d. ...fullständigt namn på alla som har tresiffrigt kundnummer?</td>
+  </tr>
+
+  	<td>4. Vad ska man skriva för fråga om man vill få fram fullständigt namn på alla kunder som med säkerhet(!) är födda i Göteborgs och Bohus län?</td>  	
+  </tr>
+  
+  <tr>
+
+</table>
+
+</tr>
+
+</body>
+</html>
--- mysql_connect_SuperServ.php
+++ mysql_connect_SuperServ.php
@@ -3,19 +3,8 @@
 	DEFINE('DB_USER','root');
 	DEFINE('DB_HOST','localhost');
 	DEFINE('DB_PASSWORD','xhtksdlolen123');
-	DEFINE('DB_DATABASE','SuperServ');
-		
-		
+	DEFINE('DB_DATABASE','test');
+				
 	$dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
 	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
 ?>
