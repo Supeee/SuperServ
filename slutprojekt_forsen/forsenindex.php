c2585fbb94bd05e9ef4644e8e6632d2d6959f860
Supeee <mcpro64@gmail.com>
2016-03-04 15:13:06 +0100

    Nya mappar

--- slutprojekt_forsen/forsenindex.php
+++ slutprojekt_forsen/forsenindex.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+		<link rel="Stylesheet" href="forsen.css" >
+	
 	<title>
-		
+		Best Forsen GachiMuchiMerch
 	</title>
 	
 </head>
 <body>
+
+	<header>The most gachimuchi store on the planet.</header>
 
 </body>
 </html>
