a7b42645379ada9941d737625d4e8f12a6e9ea9b
Supeee <mcpro64@gmail.com>
2017-04-15 15:43:38 +0200

    update2

--- www/logincheck.php
+++ www/logincheck.php
@@ -5,7 +5,6 @@ if(!@$_SESSION['loggedIn']){ // Om man inte är inloggad
 	header('Location: login.php'); // Skickas till login.php
 }
 
-
 if(@$_SESSION['timeout']+ 60*10 < time()){ //Om Sessionstiden + 10(60sekunder*10) minuter är mindre än nuvarande tid
  	session_destroy(); // Avsluta sessionen
  	session_unset(); // Avsluta sessionen (gammalt sätt)
@@ -17,5 +16,4 @@ if(@$_SESSION['timeout']+ 60*10 < time()){ //Om Sessionstiden + 10(60sekunder*10
 
 echo $meddelande; // Visa meddelande ( Hej = Inloggad | Hej då = Utloggad)
 echo $_SESSION['username'];
-
 ?>
